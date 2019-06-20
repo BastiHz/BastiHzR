4e0b595d726a77be01b0377f87e9bf838a6b403f
bastihz <41594266+BastiHz@users.noreply.github.com>
2019-06-20 14:06:43 +0200

    Expand documentation of map_range

--- R/map_range.R
+++ R/map_range.R
@@ -1,5 +1,11 @@
 #' map_range
 #'
+#' Map a vector from one range to another. Works by first scaling down to [0;1]
+#' and then scaling up to the target range.
+#'
+#' Does not throw an error if \code{out_start > out_end} because this may be desirable
+#' in some cases.
+#'
 #' @param x foo
 #' @param in_start bar
 #' @param in_end baz
@@ -15,15 +21,13 @@
 #' map_range(1:3, 0, 10)           # 0.1, 0.2, 0.3
 #' map_range(1:3, 0, 10, 0, 100)   # 10, 20, 30
 #' map_range(1:3, 1, 10, 10, 100)  # 10, 20, 30
+#' # target range may be inverted (see Details):
+#' map_range(1:3, out_start = 30, out_end = 10)  # 30, 20, 10)
 map_range <- function(x,
                       in_start = min(x, na.rm = TRUE),
                       in_end = max(x, na.rm = TRUE),
                       out_start = 0,
                       out_end = 1) {
-    # Map a vector from one range to another. Works by first scaling down to
-    # [0;1] and then scaling up to the target range.
-    # Attention: Does not throw an error if out_start > out_end because this may
-    # be desirable in some cases.
     stopifnot(
         # Prevent division by 0.
         in_start != in_end,
