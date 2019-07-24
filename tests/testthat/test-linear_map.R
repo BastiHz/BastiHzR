cc94452e43699261c0b6f39c938578bbce94a524
bastihz <41594266+BastiHz@users.noreply.github.com>
2019-07-24 11:11:01 +0200

    Include license notice in every source file

--- tests/testthat/test-linear_map.R
+++ tests/testthat/test-linear_map.R
@@ -1,3 +1,20 @@
+# BastiHzR - A personal R Package With Miscellaneous Stuff
+# Copyright (C) 2019 Sebastian Henz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
 context("linear_map")
 
 test_that("linear_map output is correct", {
@@ -7,7 +24,7 @@ test_that("linear_map output is correct", {
     expect_equal(linear_map(foo, 0, 10), c(0.1, 0.2, 0.3))
     expect_equal(linear_map(foo, 0, 10, 0, 100), c(10, 20, 30))
     expect_equal(linear_map(foo, 1, 10, 10, 100), c(10, 20, 30))
-    expect_equal(linear_map(seq(0, 360, 90), out_end = 2 * pi), 
+    expect_equal(linear_map(seq(0, 360, 90), out_end = 2 * pi),
                  seq(0, 2 * pi, pi / 2))
     expect_equal(linear_map(-1, 0, 1, 20, 30), 10)
     expect_equal(linear_map(c(1, NA, 3)), c(0, NA, 1))
