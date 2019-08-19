26de9f805d70cd530fd6310b68533a2fd18c9292
bastihz <41594266+BastiHz@users.noreply.github.com>
2019-08-19 10:11:43 +0200

    Make startupmessage less verbose

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,5 @@
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage(
-        "WARNING: Do not use BastiHzR in anything important!\n",
-        "This package is a personal playground and things will break, ",
-        "therefore nothing should depend on it too heavily."
+        "WARNING: Unstable package. Don't use it in important work."
     )
 }
