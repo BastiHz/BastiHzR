de3076293f8fb1209374245d8025ac3735803d29
Sebastian Henz <41594266+BastiHz@users.noreply.github.com>
2020-07-06 11:22:44 +0200

    Update lm_line.R

--- R/lm_line.R
+++ R/lm_line.R
@@ -7,6 +7,8 @@
 # TODO: Write tests. Test cases: positive slope, negative slope, no slope, unordered x.
 # TODO: Has to work when the variables are not called x and y. For example "mod$model$x" 
 #       will fail for other names.
+# TODO: Will get messy if there are multiple instances of a minimum or maximum x. 
+#       E.g. x values of 1, 1, 2, 3, 4, 4, 4.
 
 # lm_line <- function(mod, plot = TRUE, ...) {
 #     x <- mod$model$x
